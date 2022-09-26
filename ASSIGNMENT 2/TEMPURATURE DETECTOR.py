95704de5dc31cd39fa1ac2033e2a3aade9ddee29
muthu8870 <113693624+muthu8870@users.noreply.github.com>
2022-09-26 13:04:54 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/TEMPURATURE DETECTOR.py
@@ -0,0 +1,18 @@
+import random
+temp=list(range(0,1000))
+for i in temp:
+    valu=random.choice(temp)
+    if (valu>43):
+        print("temp is hight",valu)  
+    else:
+        print("temp is normal",valu)
+        
+    
+  
+
+
+    
+    
+    
+    
+        
